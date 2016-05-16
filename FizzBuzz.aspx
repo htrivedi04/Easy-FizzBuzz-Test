cb33a3dd005c29e99bb21d8cde87e81ca007d592
Harshit Trivedi <h.trivedi04@gmail.com>
2016-05-16 13:56:53 +0530

    Added code files for FizzBuzz

---
+++ FizzBuzz.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FizzBuzz.aspx.cs" Inherits="TestProj_FizzBuzz" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblResult" runat="server" Text="" Height="1000px"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ FizzBuzz.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class TestProj_FizzBuzz : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        FizzBuzz();
+    }
+
+
+    protected void FizzBuzz()
+    {
+        string msg = "";
+        for (int i = 1; i <= 100; i++)
+        {
+            
+            if (i % 3 == 0)
+            {
+                msg += "Fizz ";
+            }
+            if (i % 5 == 0)
+            {
+                msg += "Buzz";
+            }
+            //else msg = "";
+            if (msg == "")
+                lblResult.Text += i;
+            else lblResult.Text += msg;
+            lblResult.Text += "<br/>";
+            msg = "";
+        }
+    }
+   
+}
